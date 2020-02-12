f0f066d7f572e9406c8e65756ec8f7c955cb5fc5
David Weinstein (MCS) <daweins@microsoft.com>
2020-02-12 15:29:43 +0000

    Added 0 length file safety checks

--- laasingest/KustoHelper.cs
+++ laasingest/KustoHelper.cs
@@ -17,6 +17,19 @@ namespace laasingest
         static public void uploadBlobToKusto(CloudBlockBlob curBlob ,  string ingestConn, string dbName, string tableName, ILogger log)
         {
             log.LogInformation($"Processing blob: uri= {curBlob.Uri}");
+            // Check for 0 blob size and get rid of them
+            if ((curBlob.Properties.Length == 0) && (curBlob.Properties.LastModified < DateTime.UtcNow.AddHours(-2)))
+            {
+                log.LogInformation($"Deleting a zero length blob {curBlob.Name} with date {curBlob.Properties.LastModified}");
+                curBlob.DeleteAsync();
+                return;
+            }
+            if((curBlob.Properties.Length ==  0))
+            {
+                log.LogInformation($"Ignoring a zero length blob {curBlob.Name} with date {curBlob.Properties.LastModified}");
+                return;
+
+            }
             SharedAccessBlobPolicy blobSASPolicy = new SharedAccessBlobPolicy();
             blobSASPolicy.SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24);
             blobSASPolicy.Permissions = SharedAccessBlobPermissions.Read | SharedAccessBlobPermissions.Delete;
